78fd2afb41c07f90a99b64d5beeaacf8b129975b
David Kerr <kerrky@gmail.com>
2024-03-19 16:24:09 -0400

    Complete Challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,114 @@
 class Article:
+
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+
+        Article.all.append(self)
+
+    @property
+    def author(self):
+        return self._author
+    @author.setter
+    def author(self, author_param):
+        if(isinstance(author_param, Author)):
+            self._author = author_param
+
+    @property
+    def magazine(self):
+        return self._magazine
+    @magazine.setter
+    def magazine(self, magazine_param):
+        if(isinstance(magazine_param, Magazine)):
+            self._magazine = magazine_param
+
+    @property
+    def title(self):
+        return self._title
+    @title.setter
+    def title(self, title_param):
+        if(not hasattr(self, 'title')) and (isinstance(title_param, str)) and ( 5 <= len(title_param) <= 50):
+            self._title = title_param
         
 class Author:
+
+    all = []
+
     def __init__(self, name):
         self.name = name
 
+        Author.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name_param):
+        if(not hasattr(self, 'name')) and (isinstance(name_param, str)) and (len(name_param) > 0):
+            self._name = name_param
+
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
-
+        return list(set([article.magazine for article in Article.all if article.author == self]))
+    
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)        
 
     def topic_areas(self):
-        pass
+        if(len(self.articles()) == 0):
+            return None
+        else:
+            return [magazine.category for magazine in self.magazines()]
+        
+        
 
 class Magazine:
+
+    all = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
 
+        Magazine.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name_param):
+        if(isinstance(name_param, str)) and ( 2 <= len(name_param) <= 16):
+            self._name = name_param
+
+    @property
+    def category(self):
+        return self._category
+    @category.setter
+    def category(self, category_param):
+        if(isinstance(category_param, str)) and (len(category_param) > 0):
+            self._category = category_param
+
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine is self]
 
     def contributors(self):
-        pass
+        return list(set([article.author for article in self.articles()]))
 
     def article_titles(self):
-        pass
+        if(len(self.articles()) == 0):
+            return None
+        else:
+            return [article.title for article in self.articles()]
 
     def contributing_authors(self):
-        pass
+        authors_list = [author for author in self.contributors() if len([article for article in author.articles() if article.magazine is self]) > 2]
+        if(len(authors_list) == 0):
+            return None
+        else:
+            return authors_list
+        
--- lib/debug.py
+++ lib/debug.py
@@ -10,4 +10,19 @@ if __name__ == '__main__':
 
 
     # don't remove this line, it's for debugging!
+    magazine1 = Magazine('Vogue', 'Fashion')
+    magazine2 = Magazine('Climbing Mag', 'Sports')
+    magazine3 = Magazine('Surfing Magazine', 'Sports')
+
+    author1 = Author('Bob')
+    author2 = Author('Alice')
+    author3 = Author('Dallas')
+
+    article1 = Article(author1, magazine1, 'This is a random article about fashion')
+    article2 = Article(author2, magazine2, "New news in the climbing world!")
+    article3 = Article(author2, magazine3, "What's new in surfing!")
+    article4 = Article(author3, magazine3, "Biggest wave ever!")
+    article5 = Article(author3, magazine3, "Best starter spots in the world.")
+
+
     ipdb.set_trace()
